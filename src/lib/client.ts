db13da34060ecdaa0527ff725b99feea78ddac11
Moritz Roessler <morrossl@publicisgroupe.net>
2023-08-22 03:40:34 +0200

    fix: use real client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,15 +1,48 @@
-import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
-import { WebSocketLink } from "@apollo/client/link/ws";
-import { getMainDefinition } from "@apollo/client/utilities";
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { LOCAL_HOST } from '../config';
+
+// Create an HTTP link
+const statelessHttp = new HttpLink({
+  uri: 'https://graphql.state-less.cloud/graphql',
+});
+
+// Create a WebSocket link
+const statelessWs = new WebSocketLink({
+  uri: `wss://graphql.state-less.cloud/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+// Use the split function to direct traffic between the two links
+const stateless = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  statelessWs,
+  statelessHttp
+);
+
+// Create the Apollo Client instance
+const client = new ApolloClient({
+  link: stateless,
+  cache: new InMemoryCache(),
+});
 
 // Create an HTTP link
 const localHttp = new HttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: `http://${LOCAL_HOST}:4000/graphql`,
 });
 
 // Create a WebSocket link
 const localWs = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri: `ws://${LOCAL_HOST}:4000/graphql`,
   options: {
     reconnect: true,
   },
@@ -20,8 +53,8 @@ const local = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
     );
   },
   localWs,
@@ -34,4 +67,4 @@ export const localClient = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-export default localClient;
+export default client;
