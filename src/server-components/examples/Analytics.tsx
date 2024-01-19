ce4b57ec508fec119037825e48055da339fcb82b
Moritz Roessler <morrossl@publicisgroupe.net>
2024-01-19 10:56:49 +0100

    fix: count counters to day before if startOfDay is configured

--- src/server-components/examples/Analytics.tsx
+++ src/server-components/examples/Analytics.tsx
@@ -22,6 +22,7 @@ import {
   differenceInDays,
   differenceInMonths,
   format,
+  getHours,
   getMonth,
   startOfDay,
   startOfMonth,
@@ -112,13 +113,20 @@ export const AnalyticsPage = (props) => {
         return 'count' in todo.props && diff === 0;
       })
       .reduce((acc, todo) => {
-        const date = startOfDay(
+        const start = startOfDay(
           new Date(
             todo.props.archived
               ? todo.props.archived
               : todo.props.lastModified || todo.props.createdAt || Date.now()
           )
-        ).getTime();
+        );
+        if (
+          getHours(start) > 0 &&
+          getHours(start) < getHours(new Date(list.props.settings.startOfDay))
+        ) {
+          start.setDate(start.getDate() - 1);
+        }
+        const date = start.getTime();
         acc[date] = {
           ...acc[date],
           [todo.props.title]:
