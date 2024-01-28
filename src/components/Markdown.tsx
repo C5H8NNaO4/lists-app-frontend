63a29a0a5c23a6272cc19230c462c0547d2a8041
Moritz Roessler <moritz.roessler@gmail.com>
2024-01-28 03:34:23 +0100

    fix: update markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -9,7 +9,7 @@ import TableRow from '@mui/material/TableRow';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import { Link as RouterLink } from 'react-router-dom';
-import { useContext, useEffect, useState, createElement } from 'react';
+import { useContext, useEffect, useRef, useState, createElement } from 'react';
 
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
@@ -20,11 +20,14 @@ import copy from 'copy-to-clipboard';
 import { Actions, stateContext } from '../provider/StateProvider';
 import rehypeRaw from 'rehype-raw';
 import mermaid from 'mermaid';
+import clsx from 'clsx';
 
 type MarkdownProps = {
   children: string;
   src?: string;
   disablePadding?: boolean;
+  optimisticHeight?: string;
+  small?: boolean;
 };
 
 mermaid.initialize({
@@ -41,7 +44,13 @@ const Mermaid = (props) => {
 
   return <div className="mermaid">{props.children}</div>;
 };
-export const Markdown = ({ children, src, disablePadding }: MarkdownProps) => {
+export const Markdown = ({
+  children,
+  src,
+  disablePadding,
+  optimisticHeight = '0px',
+  small = false,
+}: MarkdownProps) => {
   const [markdown, setMarkdown] = useState<string>(children || '');
   const { state, dispatch } = useContext(stateContext);
 
@@ -70,102 +79,111 @@ export const Markdown = ({ children, src, disablePadding }: MarkdownProps) => {
   };
 
   return (
-    <ReactMarkdown
-      rehypePlugins={[rehypeRaw]}
-      remarkPlugins={[remarkGfm]}
-      components={{
-        h1: headingRenderer,
-        h2: headingRenderer,
-        h3: headingRenderer,
-        h4: headingRenderer,
-        h5: headingRenderer,
-        h6: headingRenderer,
-        pre: (props: any) => {
-          const language = (
-            props.children[0]?.props?.className || '-bash'
-          ).split('-')[1];
-
-          if (language === 'mermaid') {
-            return <Mermaid>{props.children[0].props.children}</Mermaid>;
-          }
-
-          return (
-            <>
-              <Box sx={{ width: '100%', display: 'flex' }}>
-                <IconButton
-                  sx={{ ml: 'auto', mb: -7, color: 'white' }}
-                  onClick={() => {
-                    copy(props.children[0].props.children);
-                    dispatch({
-                      type: Actions.SHOW_MESSAGE,
-                      value: 'Copied to clipboard',
-                    });
-                  }}
-                >
-                  <ContentCopyIcon />
-                </IconButton>
-              </Box>
-              <SyntaxHighlighter language={language} style={a11yDark}>
-                {props.children[0].props.children}
-              </SyntaxHighlighter>
-            </>
-          );
-        },
-        a: (props: any) => {
-          return (
-            <Link
-              sx={{
-                color: 'success.main',
-                '&:visited': { color: 'secondary.main' },
-              }}
-              to={props.href}
-              component={RouterLink}
-            >
-              {props.children}
-            </Link>
-          );
-        },
-        blockquote: (args) => {
-          return (
-            <Box
-              sx={{
-                borderLeft: '4px solid',
-                borderColor: 'primary.main',
-              }}
-            >
-              <blockquote {...args} />
-            </Box>
-          );
-        },
-        table: (props: any) => {
-          return (
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {props.children[0].props.children[0].props.children?.map(
-                    (e) => {
-                      return <TableCell>{e}</TableCell>;
-                    }
-                  )}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {props.children[1].props.children.map((row) => {
-                  return (
-                    <TableRow>
-                      {row.props.children.map((e) => {
-                        return <TableCell>{e}</TableCell>;
-                      })}
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          );
-        },
+    <div
+      style={{
+        minHeight: optimisticHeight,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'start',
+        alignContent: 'center',
       }}
     >
-      {src ? markdown : children}
-    </ReactMarkdown>
+      <ReactMarkdown
+        className={clsx({ 'markdown-small': small })}
+        rehypePlugins={[rehypeRaw]}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          h1: headingRenderer,
+          h2: headingRenderer,
+          h3: headingRenderer,
+          h4: headingRenderer,
+          h5: headingRenderer,
+          h6: headingRenderer,
+          pre: (props: any) => {
+            const language = (
+              props.children[0]?.props?.className || '-bash'
+            ).split('-')[1];
+
+            if (language === 'mermaid') {
+              return <Mermaid>{props.children[0].props.children}</Mermaid>;
+            }
+
+            return (
+              <>
+                <Box sx={{ width: '100%', display: 'flex' }}>
+                  <IconButton
+                    sx={{ ml: 'auto', mb: -7, color: 'white' }}
+                    onClick={() => {
+                      copy(props.children[0].props.children);
+                      dispatch({
+                        type: Actions.SHOW_MESSAGE,
+                        value: 'Copied to clipboard',
+                      });
+                    }}
+                  >
+                    <ContentCopyIcon />
+                  </IconButton>
+                </Box>
+                <SyntaxHighlighter language={language} style={a11yDark}>
+                  {props.children[0].props.children}
+                </SyntaxHighlighter>
+              </>
+            );
+          },
+          a: (props: any) => {
+            return (
+              <Link
+                to={props.href}
+                component={RouterLink}
+                sx={{ color: 'info.main' }}
+              >
+                {props.children}
+              </Link>
+            );
+          },
+          blockquote: (args) => {
+            return (
+              <Box
+                sx={{
+                  borderLeft: '4px solid',
+                  borderColor: 'primary.main',
+                }}
+              >
+                <blockquote {...args} />
+              </Box>
+            );
+          },
+          table: (props: any) => {
+            return (
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    {props.children[0].props.children[0].props.children?.map(
+                      (e) => {
+                        return <TableCell>{e}</TableCell>;
+                      }
+                    )}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {props.children[1].props.children.map((row) => {
+                    return (
+                      <TableRow>
+                        {row.props.children.map((e) => {
+                          return <TableCell>{e}</TableCell>;
+                        })}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            );
+          },
+        }}
+      >
+        {src ? markdown : children}
+      </ReactMarkdown>
+    </div>
   );
 };
